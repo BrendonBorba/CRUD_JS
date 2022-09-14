12b5bd929c4bc765427e390ffd0e65908fe21234
BrendonBorba <brendon_fagundes@yahoo.com.br>
2022-09-13 21:38:20 -0300

    install packets

--- app.js
+++ app.js
@@ -1,27 +1,10 @@
-var http = require('http')
+var express = require('express')
+var app = express()
+var port = 8000
 
-var server = http.createServer(function (req, res) {
-  var pagina = req.url
-  console.log(pagina)
-  if (pagina == '/contato') {
-    res.end(`
-      <!DOCTYPE html>
-        <html lang="pt-BR">
-        <head>
-          <meta charset="UTF-8">
-          <meta http-equiv="X-UA-Compatible" content="IE=edge">
-          <meta name="viewport" content="width=device-width, initial-scale=1.0">
-          <title>Document</title>
-        </head>
-        <body>
-          <h1>Pagina de Contato</h1>
-        </body>
-      </html>
-      `)
-  } else {
-    res.end(
-      `
-    <!DOCTYPE html>
+app.get('/contato', function (req, res) {
+  res.send(`
+  <!DOCTYPE html>
     <html lang="pt-BR">
     <head>
       <meta charset="UTF-8">
@@ -30,12 +13,28 @@ var server = http.createServer(function (req, res) {
       <title>Document</title>
     </head>
     <body>
-      <h1>Pagina de Home</h1>
+      <h1>Pagina de Contato</h1>
     </body>
     </html>
-    `
-    )
-  }
+    `)
 })
 
-server.listen(8000)
+app.get('/', function (req, res) {
+  res.send(`
+  <!DOCTYPE html>
+  <html lang="pt-BR">
+  <head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+  </head>
+  <body>
+    <h1>Pagina de Home</h1>
+  </body>
+  </html>`)
+})
+
+app.listen(port, function () {
+  console.log('localhost:8000')
+})
