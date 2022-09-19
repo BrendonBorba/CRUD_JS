03d5282dc2001eebfe3c5424c1854e5284931840
BrendonBorba <brendon_fagundes@yahoo.com.br>
2022-09-19 19:38:39 -0300

    add controllers

--- app/routes/routes.js
+++ app/routes/routes.js
@@ -1,19 +1,38 @@
-import { Router } from 'express'
-import clienteModel from '../controllers/clients_controller.js'
+import { Router, json } from 'express'
+import {
+  clientsIndex,
+  clientInsert,
+  clientUpdate,
+  clientDestroy,
+  clientSearch,
+  accountSearch
+} from '../controllers/clients_controller.js'
+import {
+  productsIndex,
+  productInsert,
+  productUpdate,
+  productDestroy,
+  productSearch,
+  categorySearch
+} from '../controllers/products_controller.js'
 
 const router = Router()
 
-router.get('/contato', (req, res) => {
-  res.render('site/contato')
-})
+router.use(json())
 
-router.get('/', async (req, res) => {
-  try {
-    let x = await clienteModel()
-    res.render('site/home', { clients: x })
-  } catch {
-    console.log('reusultsssss')
-  }
-})
+router
+  .get('/clients', clientsIndex)
+  .post('/client', clientInsert)
+  .put('/client/:id', clientUpdate)
+  .delete('/client/:id', clientDestroy)
+  .get('/clients/search/:fullname', clientSearch)
+  .get('/clients/search/:from/:to', accountSearch)
+
+  .get('/products', productsIndex)
+  .post('/product', productInsert)
+  .put('/product/:id', productUpdate)
+  .delete('/product/:id', productDestroy)
+  .get('/products/search/:name', productSearch)
+  .get('/products/search/:category', categorySearch)
 
 export default router
