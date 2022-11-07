81d4ede5809d4e94826fe2d995b3aab81c7e684f
BrendonBorba <brendon_fagundes@yahoo.com.br>
2022-11-07 10:06:51 -0300

    add node module part 2

---
+++ frontend/.eslintrc.js
@@ -0,0 +1,15 @@
+export default module = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: ["plugin:react/recommended", "airbnb"],
+  overrides: [],
+  parser: "babel-eslint",
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+  },
+  plugins: ["react"],
+  rules: {},
+};
